4ce2d2f3166fc1f9ed2d51eee474f0d361fbefbc
ALif <alifandianachmad@student.uir.ac.id>
2025-05-06 20:54:57 +0700

    Third commit

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,12 +1,45 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import { ref } from "vue";
+
+const tugas = ref("");
+const todos = ref([
+      {id : 1, namatugas : "membersihkan halaman", time : 17.00, done : true},
+      {id : 2, namatugas : "belajar", time : 13.00, done : false},
+      ]);
+
+  const tambahdata = () => {
+    let data = {
+      id : todos.value.length + 1,
+      namatugas : tugas.value,
+      time : new Date().getHours(),
+      done : false
+    }
+    todos.value.push(data);
+    tugas.value = "";
+  }
+
 </script>
 
 <template>
   <div class="app">
     <Navbar />
+
+
     <main class="main-content">
       <!-- Main content  -->
+      
+      <div>
+        <input v-model="tugas">
+        <button @click = "tambahdata">tambah tugas</button>
+        
+        <ul>
+          <li v-for="item in todos">
+              {{ item.namatugas }} - {{ item.time }} - {{ item.done }}
+          </li>
+        </ul>
+
+      </div>
     </main>
   </div>
 </template>
